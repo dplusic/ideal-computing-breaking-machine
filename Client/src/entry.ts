0199f2e9f8803be853263c399eec3720dd6ad3f7
Dongwoon Son <dplusic@gmail.com>
2017-09-17 17:45:49 +0900

    Sync goblin

--- Client/src/entry.ts
+++ Client/src/entry.ts
@@ -2,6 +2,7 @@ import PIXI = require("pixi.js");
 import "pixi-spine";
 import Rescale from "pixi-rescale";
 import _ = require('lodash');
+import io = require('socket.io-client');
 import { RESOURCE_CONFIG_URL } from "webpack-game-asset-plugin/helper";
 import GOBLIN_ATLAS from "game-asset!assets/goblins_atlas.atlas";
 import GOBLIN_IMAGE from "game-asset!assets/goblins_img.png";
@@ -44,7 +45,8 @@ async function load() {
 load().then(main);
 
 const resources = PIXI.loader.resources;
-async function main() {
+
+function addGoblin() {
     const spineAtlas = new PIXI.spine.core.TextureAtlas(resources[GOBLIN_ATLAS].data, (__, cb) => cb(PIXI.BaseTexture.from(GOBLIN_IMAGE)));
     const spineAtlasLoader = new PIXI.spine.core.AtlasAttachmentLoader(spineAtlas);
     const spineJsonParser = new PIXI.spine.core.SkeletonJson(spineAtlasLoader);
@@ -54,4 +56,58 @@ async function main() {
     goblin.position.set(app.renderer.width / 2, app.renderer.height / 2);
     goblin.autoUpdate = true;
     app.stage.addChild(goblin);
+    return goblin;
+}
+
+function syncGoblin(goblin: PIXI.spine.Spine, pos: {x: number, y: number}, bones: Array<{x: number, y: number, rotation: number}>) {
+    const goblinSpine = goblin;
+    goblinSpine.x = pos.x;
+    goblinSpine.y = pos.y;
+    goblinSpine.skeleton.slots.map((e, i) => {
+        const bone = bones[i];
+        e.bone.x = bone.x;
+        e.bone.y = bone.y;
+        e.bone.rotation = bone.rotation;
+    });
+}
+
+const otherGoblins: { [key:string]:PIXI.spine.Spine; } = {};
+
+function getOtherGoblin(id: string) {
+    if (id in otherGoblins) {
+        return otherGoblins[id];
+    } else {
+        const goblin = addGoblin();
+        otherGoblins[id] = goblin;
+        return goblin;
+    }
+}
+
+function connect(goblin: PIXI.spine.Spine) {
+    const socket = io('http://192.168.0.111');
+
+    socket.on('login', (id: string) => {
+        app.ticker.add(dt => {
+            const pos = {x: goblin.x, y: goblin.y};
+            const bones = goblin.skeleton.slots.map(x => ({x: x.bone.x, y: x.bone.y, rotation: x.bone.rotation}));
+
+            socket.emit('update bone', id, pos, bones);
+        });
+    });
+
+    socket.on('sync bone', (id: string, pos: {x: number, y: number}, bones: Array<{x: number, y: number, rotation: number}>) => {
+        const goblin = getOtherGoblin(id);
+        syncGoblin(goblin, pos, bones);
+    });
+}
+
+async function main() {
+    const goblin = addGoblin();
+
+    goblin.state.setAnimation(0, 'walk', true);
+    goblin.x = _.random(0, 1000);
+
+    (window as any).goblin = goblin;
+
+    connect(goblin);
 }
