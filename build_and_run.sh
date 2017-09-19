143fed988f96fda7fff891c18b41d5d9be6c4254
Dongwoon Son <dplusic@gmail.com>
2017-09-19 11:33:59 +0900

    Add build_and_run.sh

---
+++ build_and_run.sh
@@ -0,0 +1,13 @@
+echo '##########'
+echo '## ICBM ##'
+echo '##########'
+echo '--- NOTE ---'
+echo '* Change server url at Client/src/entry.ts:104'
+echo '------------'
+
+cd Client
+npm install
+node_modules/.bin/webpack ../server/client.js
+cd ../server
+npm install
+node server.js
